46bdbc7d92d85cb97c171bccaf8bd8a0b782f616
Tanya Gray <153077+tanyagray@users.noreply.github.com>
2021-01-24 20:48:22 +1300

    use newline character instead of empty print functions

--- app/app.py
+++ app/app.py
@@ -40,8 +40,7 @@ class Product(Base):
 FUNCTIONS
 """
 def list_all_products():
-  print()
-  print('All products:')
+  print('\nAll products:')
   
   session = Session()
   products = session.query(Product).all()
@@ -49,29 +48,24 @@ def list_all_products():
   for product in products:
     print(product)
 
-  print()
-  input('Press Enter to return to menu...')
+  input('\nPress Enter to return to menu...')
 
 
 def search_for_product():
-  print()
-  search_term = input('Please enter a search term: ')
+  search_term = input('\nPlease enter a search term: ')
 
   session = Session()
   products = session.query(Product).filter(Product.name.like('%'+search_term+'%')).all()
 
-  print()
-  print('Matching products:')
+  print('\nMatching products:')
 
   for product in products:
     print(product)
   
-  print()
-  input('Press Enter to return to menu...')
+  input('\nPress Enter to return to menu...')
 
 def add_new_product():
-  print()
-  print('Add a new product:')
+  print('\nAdd a new product:')
 
   name = input('What is the product name? ')
   desc = input('What is the description? ')
@@ -89,11 +83,10 @@ def add_new_product():
   session.add(new_product)
   session.commit()
 
-  print('Added new product:')
+  print('\nAdded new product:')
   print(name + ': ' + description + ' $' + price + ' (' + stock + ')')
 
-  print()
-  input('Press Enter to return to menu...')
+  input('\nPress Enter to return to menu...')
 
 
 """
@@ -103,14 +96,13 @@ MAIN MENU
 print('Hello! Welcome to the Cake Shop!')
 
 while True:
-  print()
-  print('What would you like to do?')
+  print('\nWhat would you like to do?')
   print('1. List all products')
   print('2. Search for a product')
   print('3. Add a new product')
   print('4. Quit')
 
-  user_choice = input('Please enter a number: ')
+  user_choice = input('\nPlease enter a number: ')
 
   if user_choice == '1':
     list_all_products()
@@ -119,7 +111,7 @@ while True:
   elif user_choice == '3':
     add_new_product()
   elif user_choice == '4':
-    print('Goodbye!')
+    print('\nGoodbye!')
     sys.exit(0)
   else:
-    print('Please enter a valid number')
+    print('\nSorry that\'s not a valid number')
